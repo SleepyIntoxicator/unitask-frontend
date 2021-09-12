b6b2bc4553f914415d666be3f0ae4867001b06af
SleepyIntoxicator <Alsakharov@mail.ru>
2021-09-12 11:43:24 +0300

    added base landing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
+
+const Landing = () => import('@/base/pages/Landing.vue');
+const AboutVue = () => import('@/views/Home.vue');
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Landing',
+    component: Landing,
   },
   {
     path: '/about',
@@ -14,6 +16,11 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/about/vue',
+    name: 'About vue',
+    component: AboutVue,
   },
 ];
 
