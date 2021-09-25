bb98394c52826399a302b9f84db71c44ed29302b
SleepyIntoxicator <Alsakharov@mail.ru>
2021-09-25 04:22:27 +0300

    Added support for the material-icons

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
-import { createApp } from 'vue';
 import { Quasar } from 'quasar';
+import lang from 'quasar/lang/en-US';
+import 'quasar/dist/quasar.sass';
+
+import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import 'quasar/dist/quasar.sass';
-
-createApp(App).use(Quasar).use(store).use(router)
+createApp(App)
+  .use(Quasar, {
+    lang,
+    extras: ['material-icons',
+      'material-icons-outlined',
+      'material-icons-round',
+      'material-icons-sharp'],
+  })
+  .use(store)
+  .use(router)
   .mount('#app');
