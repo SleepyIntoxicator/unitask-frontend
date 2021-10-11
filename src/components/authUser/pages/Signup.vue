abd5676d5c114dcae0944bfe6125a44a4eb278ff
SleepyIntoxicator <Alsakharov@mail.ru>
2021-10-11 11:21:00 +0300

    Added Signup.vue component

---
+++ src/components/authUser/pages/Signup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Signup</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Signup',
+});
+</script>
+
+<style scoped>
+
+</style>
