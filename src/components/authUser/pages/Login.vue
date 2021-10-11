55285f2706ddcfb009800e2cfbba5475a3d9955e
SleepyIntoxicator <Alsakharov@mail.ru>
2021-10-11 11:20:45 +0300

    Added Login.vue component

---
+++ src/components/authUser/pages/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <q-form
+      name="login"
+      @submit.prevent="loginSubmit">
+      <q-input model-value="login"></q-input>
+      <q-input model-value="password"></q-input>
+      <q-btn label="Login"></q-btn>
+    </q-form>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Login',
+  data() {
+    return {
+      loginData: {
+        login: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    loginSubmit() {
+      this.$emit('onLogin', {
+        loginData: {
+          login: this.loginData.login,
+          password: this.loginData.password,
+        },
+      });
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
