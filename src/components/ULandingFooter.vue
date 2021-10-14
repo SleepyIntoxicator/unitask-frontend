35b43a547721bcb4724c3c86c2b69c49270efb08
SleepyIntoxicator <Alsakharov@mail.ru>
2021-10-14 17:05:52 +0300

    Added landing footer in the ULandingFooter component

---
+++ src/components/ULandingFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-footer class="bg-grey-8 text-white">
+    <div>Unitask {{ currentYear }} by Slintox '
+      Powered on Quasar
+      <q-badge>{{ this.$q.version }}</q-badge>
+    </div>
+  </q-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ULandingFooter',
+  computed: {
+    currentYear(): number {
+      const dt = new Date();
+      return dt.getFullYear();
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
