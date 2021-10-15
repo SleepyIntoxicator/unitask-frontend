27e48e90e7259532c3191038147d53c5a0c30286
SleepyIntoxicator <Alsakharov@mail.ru>
2021-10-15 21:14:04 +0300

    Added custom layout.

---
+++ src/layouts/CustomLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout>
+    <router-view></router-view>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CustomLayout',
+});
+</script>
+
+<style scoped>
+
+</style>
