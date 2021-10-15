1c42056d513ae5ad624b9244f6720921faacb99d
SleepyIntoxicator <Alsakharov@mail.ru>
2021-10-15 21:51:35 +0300

    Added home page

---
+++ src/components/homePage/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page-container>
+    <h3>Home page</h3>
+  </q-page-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomePage',
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
