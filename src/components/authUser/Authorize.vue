253fd7b392b79665ebcb16905b90cf85cd9a9caa
SleepyIntoxicator <Alsakharov@mail.ru>
2021-10-15 21:16:45 +0300

    Added a single authorize page with login and signup on the same one.

---
+++ src/components/authUser/Authorize.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Authorize',
+  data() {
+    return {
+      loginData: {
+        login: '',
+        password: '',
+      },
+      signupData: {
+        login: '',
+        email: '',
+        fullName: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    loginSubmit() {
+      this.$emit('onLogin', {
+        loginData: {
+          login: this.loginData.login,
+          password: this.loginData.password,
+        },
+      });
+    },
+    signupSubmit() {
+      this.$emit('onSignup', {
+        signupData: {
+          login: this.signupData.login,
+          email: this.signupData.email,
+          fullName: this.signupData.fullName,
+          password: this.signupData.password,
+        },
+      });
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
