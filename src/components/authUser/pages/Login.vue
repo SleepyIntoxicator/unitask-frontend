64bd7116bdbe64ed9974941eda1fa36052438a46
SleepyIntoxicator <Alsakharov@mail.ru>
2021-10-15 21:25:26 +0300

    Added styles and logic for login input components.

--- src/components/authUser/pages/Login.vue
+++ src/components/authUser/pages/Login.vue
@@ -1,22 +1,44 @@
 <template>
-  <div>
-    <q-form
-      name="login"
-      @submit.prevent="loginSubmit">
-      <q-input model-value="login"></q-input>
-      <q-input model-value="password"></q-input>
-      <q-btn label="Login"></q-btn>
-    </q-form>
-  </div>
+    <q-page-container class="login-container justify-center">
+      <q-form
+        class="login-form"
+        name="login"
+        @submit="loginSubmit">
+        <q-input
+          placeholder="Login or email"
+          v-model="loginData.login"
+          outlined
+          class="login-from-input"
+        ></q-input>
+        <q-input
+          v-model="loginData.password"
+          :type="isPwd ? 'password' : 'text'"
+          key="password"
+          name="password"
+          placeholder="Password"
+          outlined
+          class="login-from-input"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"/>
+          </template>
+        </q-input>
+        <q-btn type="submit" label="Login"></q-btn>
+      </q-form>
+    </q-page-container>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'Login',
   data() {
     return {
+      isPwd: ref(true),
       loginData: {
         login: '',
         password: '',
@@ -31,11 +53,36 @@ export default defineComponent({
           password: this.loginData.password,
         },
       });
+      console.log('login l:', this.loginData.login, ' p: ', this.loginData.password);
+      this.$router.push('/');
     },
   },
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.login-container {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+  background-color: #E7EAEF;
+  display: flex;
+  color: #404247;
+  justify-content: center;
+  align-items: center;
+  /*width: 30%; */
+}
 
+.login-form {
+  position: relative;
+  width: 320px;
+  min-width: 300px;
+  padding: 6% 30px 12%;
+  z-index: 1;
+}
+
+.login-form .login-from-input {
+  margin-bottom: 10px;
+}
 </style>
